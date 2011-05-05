dbe17f42b5cd0a2eb4168ce1795e2d878b70c80c
Phil Calçado <pcalcado@gmail.com>
2011-05-05 09:46:31 -0400

    added failing test case for c.c.profile when profile data has values larger than ints

--- modules/profile/src/test/clojure/clojure/contrib/test_profile.clj
+++ modules/profile/src/test/clojure/clojure/contrib/test_profile.clj
@@ -6,3 +6,14 @@
   (testing "doesn't blow up with no data (assembla #31)"
     (is (= "Name      mean       min       max     count       sum\n"
            (with-out-str (print-summary {}))))))
+
+(deftest can-summarize-profile-data
+  (testing "summarizing a quick method"
+    (is (= {:really-quick-operation {:mean 6, :min 0, :max 40, :count 7, :sum 46}}
+           (summarize {:really-quick-operation (list 0 0 1 1 2 2 40)}))))
+
+  (testing "summarizing a slow method"
+    (let [long-time (+ Integer/MAX_VALUE 1)]
+     (is (= {:really-quick-operation {:mean long-time, :min long-time, :max long-time, :count 1, :sum long-time}}
+            (summarize {:really-slow-operation (list  long-time)}))))))
+
